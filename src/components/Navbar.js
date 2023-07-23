8f2f4275813de97650d75e1466c4546c790d8424
Omar Abotahoon <oabot040@uottawa.ca>
2023-07-22 20:29:51 -0400

    deployment troubleshooting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,28 +10,28 @@ export default function Navbar(props) {
             <nav className="navbar" role="navigation">
                 <div className="container-fluid">
                     <div>
-                        <a href="/meteorFC#home-section">
+                        <a href="/#home-section">
                             <img src={require("../images/logo/logo1.png")} alt="Logo-picture" className="logo"/>
                         </a>                        
                     </div>
                     <ul className="nav--items">
                         <li>
-                           <a href="/meteorFC#home-section" className="navbar-link">{props.french ? "Page d'accueil" : "Home"}</a> 
+                           <a href="/#home-section" className="navbar-link">{props.french ? "Page d'accueil" : "Home"}</a> 
                         </li>
                         <li>
-                            <a href="/meteorFC#programs-section" className="navbar-link">{props.french ? "Programmes et ligues" : "Programs and Leagues"}</a>
+                            <a href="/#programs-section" className="navbar-link">{props.french ? "Programmes et ligues" : "Programs and Leagues"}</a>
                         </li>
                         <li>
-                            <a href="/meteorFC#fields-section" className="navbar-link">{props.french ? "Des champs" : "Fields"}</a>
+                            <a href="/#fields-section" className="navbar-link">{props.french ? "Des champs" : "Fields"}</a>
                         </li>
                         <li>
-                            <a href="/meteorFC#team-section" className="navbar-link">{props.french ? "Équipe" : "Team"}</a>
+                            <a href="/#team-section" className="navbar-link">{props.french ? "Équipe" : "Team"}</a>
                         </li>
                         <li>
-                            <a href="/meteorFC#contact-section" className="navbar-link">{props.french ? "Contact" : "Contact"}</a>
+                            <a href="/#contact-section" className="navbar-link">{props.french ? "Contact" : "Contact"}</a>
                         </li>
                         <li>
-                            <a href="/meteorFC#gallery-section" className="navbar-link">{props.french ? "Galerie" : "Gallery"}</a>
+                            <a href="/#gallery-section" className="navbar-link">{props.french ? "Galerie" : "Gallery"}</a>
                         </li>
                         <FormControlLabel control={<Switch onChange={props.changeLanguage}/>} label="Fr" labelPlacement="start" className="lang-switch"/>
                     </ul>
